76c284c529422858120bac6f98c93e32cc86d415
Todd Turner <todd.turner@utoronto.ca>
2015-04-11 19:36:14 -0400

    - Basic implementation of a top level object.  Presently creates an empty object         new file:   .gitignore 	new file:   topLevelObject.js

---
+++ topLevelObject.js
@@ -0,0 +1,13 @@
+function TopLevelObject(config) {
+    if(!(this instanceof TopLevelObject) {
+        return new TopLevelObject(config);
+    }
+
+    var topLevelObject = {};
+
+    
+
+    return topLevelObj;
+}
+
+module.exports = topLevelObject;
